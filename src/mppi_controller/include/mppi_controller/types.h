8cffa2ae789ee4fe310c5c90576b7f754b7317bf
Daniel M. Sahu <danielmohansahu@gmail.com>
2022-04-05 16:30:51 -0400

    Added common 'global' definitions.

---
+++ src/mppi_controller/include/mppi_controller/types.h
@@ -0,0 +1,30 @@
+/* types.h
+ *
+ * Common definitions used across this package.
+ */
+
+#pragma once
+
+#include <eigen3/Eigen/Core>
+
+namespace mppi
+{
+
+// state is represented by (x, y, theta, dx, dy, dtheta)
+constexpr size_t STATE_DIM = 6;
+
+// position is represented by (x, y, theta)
+constexpr size_t POS_DIM = 3;
+constexpr size_t VEL_DIM = 3;
+
+// control is represented by (left wheel angular vel, right wheel angular vel)
+constexpr size_t CONTROL_DIM = 2;
+
+// Array shorthands
+using Statef    = Eigen::Matrix<float, STATE_DIM, 1>;
+using Controlf  = Eigen::Matrix<float, CONTROL_DIM, 1>;
+
+// convenience typedefs of common types
+using Matrix    = Eigen::MatrixXf;
+
+} // namespace mppi
