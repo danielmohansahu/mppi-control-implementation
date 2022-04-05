ffdd449b97854feadd89f3544ea628652ddaaa07
Daniel M. Sahu <danielmohansahu@gmail.com>
2022-04-05 16:31:36 -0400

    Initial attempt at implementing analytical forward model.

--- src/mppi_controller/include/mppi_controller/forward_model.h
+++ src/mppi_controller/include/mppi_controller/forward_model.h
@@ -5,11 +5,42 @@
 
 #pragma once
 
+// STL
+#include <atomic>
+
+// Eigen
+#include <eigen3/Eigen/Core>
+
+// custom
+#include "types.h"
+
 namespace mppi
 {
 
-class ForwardModel
+/* skid steer platform friction model
+ * 
+ * https://www.joydeepb.com/Publications/icra2019_skid_steer.pdf
+ */
+struct ForwardModel
 {
+  // nominal Jackal physical parameters
+  std::atomic<float> wheel_radius {0.098};
+  std::atomic<float> wheel_seperation {0.262};
+
+  // time-dependent coefficients
+  //  defaults result in a simple friction-free kinematic model
+  std::atomic<float> icr {1.0};
+  std::atomic<float> slip_left {0.0};
+  std::atomic<float> slip_right {0.0};
+
+  // simulate control delay
+  std::atomic<float> delay {0.0};
+
+  // simulation specific parameters
+  std::atomic<float> dt {0.02};
+
+  // predict the output of the given control sequence
+  Eigen::MatrixXf rollout(const Eigen::Ref<Statef> state, const Eigen::Ref<Matrix> commands) const;
 };
 
 } // namespace mppi
