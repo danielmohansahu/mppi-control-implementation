2484678a51a65110806d07e72f3e16652cc0c693
Daniel M. Sahu <danielmohansahu@gmail.com>
2022-04-06 17:16:00 -0400

    Added a marker to visualize the current goal.

--- src/mppi_controller/scripts/send_goal.py
+++ src/mppi_controller/scripts/send_goal.py
@@ -10,6 +10,7 @@ import argparse
 # ROS
 import rospy
 from geometry_msgs.msg import PoseStamped
+from visualization_msgs.msg import Marker
 
 def parse_args():
     """ Parse input arguments.
@@ -17,6 +18,8 @@ def parse_args():
     parser = argparse.ArgumentParser("Specify Goal arguments.")
     parser.add_argument("-t", "--topic", default="jackal/goal",
                         help="Name of the topic on which to publish.")
+    parser.add_argument("-m", "--marker-topic", default="jackal/goal_marker",
+                        help="Name of the marker topic on which to publish.")
     parser.add_argument("-f", "--frame-id", default="jackal/odom",
                         help="Coordinate frame of the goal.")
     parser.add_argument("-x", "--x", default=0.0, type=float,
@@ -34,8 +37,9 @@ if __name__ == "__main__":
     # parse input arguments
     args = parse_args()
 
-    # create publisher
+    # create publishers
     publisher = rospy.Publisher(args.topic, PoseStamped, queue_size=1)
+    marker_pub = rospy.Publisher(args.marker_topic, Marker, queue_size=1, latch=True)
 
     # create goal
     goal = PoseStamped()
@@ -44,10 +48,25 @@ if __name__ == "__main__":
     goal.pose.position.x = args.x
     goal.pose.position.y = args.y
 
+    # create marker
+    marker = Marker()
+    marker.header = goal.header
+    marker.type = Marker.CYLINDER
+    marker.action = Marker.ADD
+    marker.scale.x = 1.0
+    marker.scale.y = 1.0
+    marker.scale.z = 0.01
+    marker.pose.position.x = args.x
+    marker.pose.position.y = args.y
+    marker.pose.orientation.w = 1.0
+    marker.color.b = 1.0
+    marker.color.a = 1.0
+
     # sleep a tiny bit to make sure everything is connected. Rospy is weird.
     rospy.sleep(0.5)
 
     rospy.loginfo("Sending goal: \n{}".format(goal))
     publisher.publish(goal)
+    marker_pub.publish(marker)
 
     # and, we're done.
