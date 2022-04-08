d9aa8e91e24e633a0f3dc0c0746b886c5ea874fe
Daniel M. Sahu <danielmohansahu@gmail.com>
2022-04-08 19:03:14 -0400

    Updated demo script to use an action client instead of a publisher.

--- src/mppi_demo/scripts/send_goal.py
+++ src/mppi_demo/scripts/send_goal.py
@@ -9,6 +9,8 @@ import argparse
 
 # ROS
 import rospy
+import actionlib
+from mppi_controller.msg import WaypointAction, WaypointGoal
 from geometry_msgs.msg import PoseStamped
 from visualization_msgs.msg import Marker
 
@@ -16,8 +18,8 @@ def parse_args():
     """ Parse input arguments.
     """
     parser = argparse.ArgumentParser("Specify Goal arguments.")
-    parser.add_argument("-t", "--topic", default="jackal/goal",
-                        help="Name of the topic on which to publish.")
+    parser.add_argument("-n", "--server-name", default="jackal/waypoint",
+                        help="Name of the action server to look for.")
     parser.add_argument("-m", "--marker-topic", default="jackal/goal_marker",
                         help="Name of the marker topic on which to publish.")
     parser.add_argument("-f", "--frame-id", default="jackal/odom",
@@ -27,7 +29,9 @@ def parse_args():
     parser.add_argument("-y", "--y", default=0.0, type=float,
                         help="Y location of the goal.")
     parser.add_argument("-r", "--radius", default=1.0, type=float,
-                        help="Radius of goal (for visualization; not actually used by controller).")
+                        help="Radius of goal.")
+    parser.add_argument("-t", "--timeout", default=0.0, type=float,
+                        help="Timeout for the goal; values <= 0.0 will be interpreted as infinity.")
     
     args, _ = parser.parse_known_args()
     return args
@@ -39,20 +43,26 @@ if __name__ == "__main__":
     # parse input arguments
     args = parse_args()
 
-    # create publishers
-    publisher = rospy.Publisher(args.topic, PoseStamped, queue_size=1)
+    # create action client
+    client = actionlib.SimpleActionClient(args.server_name, WaypointAction)
+    rospy.loginfo("Waiting (indefinitely) for action server {}".format(args.server_name))
+    client.wait_for_server()
+
+    # create publisher for marker
     marker_pub = rospy.Publisher(args.marker_topic, Marker, queue_size=1, latch=True)
 
     # create goal
-    goal = PoseStamped()
-    goal.header.stamp = rospy.Time.now()
-    goal.header.frame_id = args.frame_id
-    goal.pose.position.x = args.x
-    goal.pose.position.y = args.y
+    goal = WaypointGoal()
+    goal.pose.header.stamp = rospy.Time.now()
+    goal.pose.header.frame_id = args.frame_id
+    goal.pose.pose.position.x = args.x
+    goal.pose.pose.position.y = args.y
+    goal.radius = args.radius
+    goal.timeout = args.timeout
 
     # create marker
     marker = Marker()
-    marker.header = goal.header
+    marker.header = goal.pose.header
     marker.type = Marker.CYLINDER
     marker.action = Marker.ADD
     marker.scale.x = 2.0 * args.radius
@@ -64,11 +74,14 @@ if __name__ == "__main__":
     marker.color.b = 1.0
     marker.color.a = 1.0
 
-    # sleep a tiny bit to make sure everything is connected. Rospy is weird.
-    rospy.sleep(0.5)
-
     rospy.loginfo("Sending goal: \n{}".format(goal))
-    publisher.publish(goal)
+    client.send_goal(goal)
     marker_pub.publish(marker)
 
-    # and, we're done.
+    # wait for success (or failure)
+    client.wait_for_result()
+    result = client.get_result()
+
+    rospy.loginfo("Server {}".format("succeeded!" if result.success else "failed."))
+
+    # and, we're done
