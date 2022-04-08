ada88a60e73e97beef6b2b66cd79c623fcc17487
Daniel M. Sahu <danielmohansahu@gmail.com>
2022-04-08 19:18:00 -0400

    Randomizing goal if not explicitly specified.

--- src/mppi_demo/scripts/send_goal.py
+++ src/mppi_demo/scripts/send_goal.py
@@ -5,6 +5,7 @@ A simple utility script to execute a goal.
 """
 
 # STL
+import random
 import argparse
 
 # ROS
@@ -24,17 +25,26 @@ def parse_args():
                         help="Name of the marker topic on which to publish.")
     parser.add_argument("-f", "--frame-id", default="jackal/odom",
                         help="Coordinate frame of the goal.")
-    parser.add_argument("-x", "--x", default=0.0, type=float,
+    parser.add_argument("-x", "--x", default=None, type=float,
                         help="X location of the goal.")
-    parser.add_argument("-y", "--y", default=0.0, type=float,
+    parser.add_argument("-y", "--y", default=None, type=float,
                         help="Y location of the goal.")
     parser.add_argument("-r", "--radius", default=1.0, type=float,
                         help="Radius of goal.")
+    parser.add_argument("-v", "--velocity", default=2.0, type=float,
+                        help="Target linear velocity (m/s).")
     parser.add_argument("-t", "--timeout", default=0.0, type=float,
                         help="Timeout for the goal; values <= 0.0 will be interpreted as infinity.")
     
     args, _ = parser.parse_known_args()
     return args
+
+def random_if_none(x):
+    """ Return the value itself if it's not None, else a random number.
+    """
+    if x is not None:
+        return x
+    return 10.0 * (random.random() - 0.5)
 
 if __name__ == "__main__":
     # initialize node
@@ -55,8 +65,9 @@ if __name__ == "__main__":
     goal = WaypointGoal()
     goal.pose.header.stamp = rospy.Time.now()
     goal.pose.header.frame_id = args.frame_id
-    goal.pose.pose.position.x = args.x
-    goal.pose.pose.position.y = args.y
+    goal.pose.pose.position.x = random_if_none(args.x)
+    goal.pose.pose.position.y = random_if_none(args.y)
+    goal.velocity = args.velocity
     goal.radius = args.radius
     goal.timeout = args.timeout
 
@@ -68,8 +79,8 @@ if __name__ == "__main__":
     marker.scale.x = 2.0 * args.radius
     marker.scale.y = 2.0 * args.radius
     marker.scale.z = 0.01
-    marker.pose.position.x = args.x
-    marker.pose.position.y = args.y
+    marker.pose.position.x = goal.pose.pose.position.x
+    marker.pose.position.y = goal.pose.pose.position.y
     marker.pose.orientation.w = 1.0
     marker.color.b = 1.0
     marker.color.a = 1.0
