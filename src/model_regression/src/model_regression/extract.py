9eca028dd72a9d3167dbaacdd003f2e3b94659bc
Daniel M. Sahu <danielmohansahu@gmail.com>
2022-04-09 19:32:55 -0400

    Again working through various bugs; not verified.

--- src/model_regression/src/model_regression/extract.py
+++ src/model_regression/src/model_regression/extract.py
@@ -10,6 +10,7 @@ the driving independent variables.
 # STL
 import os
 import csv
+from collections import OrderedDict
 
 # ROS
 import rosbag
@@ -17,17 +18,12 @@ import rosbag
 class Extractor:
     """ Object Oriented class for extracting parameters + costs from a given bag.
     """
-    def __init__(self, params, params_topic, goal_topic, result_topic, odom_topic, cmd_topic, output, cost_function):
-        # expected independent parameters
-        #  sort to make sure we write this in the correct order later!
-        self.params = params
-        self.params.sort()
-
+    def __init__(self, params_topic, goal_topic, result_topic, odom_topic, cmd_topic, output, cost_function):
         # expected topics for param / cost extraction
         self.topics = {
             "params": params_topic,
-            "goal": goals_topic,
-            "result": results_topic,
+            "goal": goal_topic,
+            "result": result_topic,
             "odom": odom_topic,
             "cmd": cmd_topic
         }
@@ -47,8 +43,9 @@ class Extractor:
         """
 
         # perform sanity checks on bag
-        if not os.file.exists(filepath):
-            raise RuntimeError("Given non-existent filename '{}'".format(filename))
+        if not os.path.exists(filepath):
+            print("\tgiven non-existent filename '{}'".format(filename))
+            return False
 
         # load bag metadata
         bag = rosbag.Bag(filepath, "r")
@@ -57,38 +54,60 @@ class Extractor:
         # check that all expected topics exist
         for topic in self.topics.values():
             if topic not in topics_info:
-                raise RuntimeError("Bag '{}' does not contain required topic '{}' !")
+                print("\tbag '{}' does not contain required topic '{}' !".format(filepath, topic))
+                return False
 
         # iterate through the bag, collecting relevant topics
         data = {key: [] for key in self.topics.keys()}
-        for topic,msg,_ in bag.read_messages(topics=list(self.topics.keys())):
-            data[topic].append(msg)
+        for topic,msg,_ in bag.read_messages():
+            if topic not in self.topics.values():
+                continue
+            key = next(k for k,t in self.topics.items() if t == topic)
+            data[key].append(msg)
 
         # sanity check number of topics
-        for item in ["params", "goal", "result"]:
+        if len(data["params"]) != 2:
+            print("\t unexpected number of messages for topic '{}' in '{}'".format(self.topics["params"], filepath))
+            return False
+
+        for item in ["goal", "result"]:
             topic = self.topics[item]
-            if len(data[topic]) != 1:
-                print("Unexpected number of messages for topic '{}'".format(topic))
-                import pdb;pdb.set_trace()
+            if len(data[item]) != 1:
+                print("\tunexpected number of messages for topic '{}' in '{}'".format(topic, filepaht))
+                return False
 
         # @TODO extract from message
-        import pdb;pdb.set_trace()
-        params = []
+        unsorted_params = []
+        for type_ in ["bools", "ints", "strs", "doubles"]:
+            for val in getattr(data["params"][-1], type_):
+                unsorted_params.append((val.name, val.value))
+        unsorted_params.sort()
+        params = OrderedDict({k:v for k,v in unsorted_params})
 
         # get total cost from the other topics
-        cost = self.cost_function(goal=self.topics["goal"][0], result=self.topics["result"][0], odom=self.topics["odom"], cmd=self.topics["cmd"])
+        cost = self.cost_function(goal=data["goal"][0], result=data["result"][0], odom=data["odom"], cmd=data["cmd"])
 
         # write to file
-        write(filepath.split("/")[-1], params, cost)
+        self.write(filepath.split("/")[-1], params, cost)
 
+        # indicate success
+        return True
 
     def write(self, filename, params, cost):
         """ Write a new line to the given output file.
 
         We assume the given params are in the correct order.
         """
+        first_time = os.path.getsize(self.output) == 0
+
         with open(self.output, "a") as csvfile:
-            writer = csv.writer(csvfile, delimeter=",")
-            writer.writerow([filename] + params + [cost])
+            writer = csv.writer(csvfile, delimiter=",")
+
+            # write header iff this is our first line
+            if first_time:
+                header = ["filename"] + list(params.keys()) + ["cost"]
+                writer.writerow(header)
+
+            writer.writerow([filename] + list(params.values()) + [cost])
 
 
